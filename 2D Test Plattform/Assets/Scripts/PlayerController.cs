807af517c1890bf83f0740ff93af9ed646b658fa
FapianoSWE <33018606+FapianoSWE@users.noreply.github.com>
2018-03-21 23:36:22 +0100

    Movement, Level generation

---
+++ 2D Test Plattform/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Gamestates
+{
+    alive,
+    dead
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float speed = 10f;
+    public float jumpHeight = 20f;
+    public bool grounded = false;
+    public bool isMoving = false;
+    public bool turnedRight = true;
+    public Vector2 currentVelocity;
+    Rigidbody2D rb;
+
+    Gamestates gameStates = new Gamestates();
+
+    public GameObject[] currentTeam;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = GameObject.Find("Spawn").transform.position;
+            rb.velocity = Vector2.zero;
+        }
+        if (rb.velocity.y > 0 || rb.velocity.y < 0)
+        {
+            grounded = false;
+        }
+        if (rb.velocity.x == 0)
+        {
+            isMoving = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (gameStates == Gamestates.alive)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "HostileTerrain")
+        {
+            gameStates = Gamestates.dead;
+        }
+        if (collision.collider.tag == "Terrain" && rb.velocity.y == 0)
+        {
+            grounded = true;
+        }
+    }
+}
